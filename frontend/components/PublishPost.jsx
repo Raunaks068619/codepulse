d64f09383147a11b6e0d0b83f3d8684f9cc2451e
Priti Gediya <pritigediya@fynd-external.com>
2025-02-06 19:20:14 +0530

    Implemented Publish post api integration

--- frontend/components/PublishPost.jsx
+++ frontend/components/PublishPost.jsx
@@ -1,9 +1,12 @@
 import {
-  Box,
   TextField,
   Button,
-  Typography,
-  TextareaAutosize,
+  Select,
+  MenuItem,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  InputLabel,
 } from "@mui/material";
 import InstaPost from "./InstaPost";
 import "./style/GenerateCaption.css";
@@ -11,6 +14,8 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./style/PublishPost.css";
+import { CTAtitles } from "../constants";
 
 const PublishPost = ({
   authData,
@@ -20,34 +25,64 @@ const PublishPost = ({
   moveToBackStep = () => {},
 }) => {
   const { company_id, application_id } = useParams();
-  const [productDescription, setProductDescription] = useState("");
-  const [productCaption, setProductCaption] = useState("");
-  const [productHashtag, setProductHashtag] = useState([]);
+  const [catTitle, setCatTitle] = useState("");
+  const [budgetAmount, setBudgetAmount] = useState();
+  const [minAge, setMinAge] = useState("");
+  const [maxAge, setMaxAge] = useState("");
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    setProductDescription(progressData?.selectedProduct?.description);
+    // setProductDescription(progressData?.selectedProduct?.description);
   }, [progressData]);
 
   const storeAndmoveToNextStep = async () => {
     try {
-      const response = await axios.post("/api/instagram/post", {
-        imageUrl:
-          "https://cdn.pixelbin.io/v2/potlee/t.resize(h:1080,w:1080)/1200px-Square_Inc_-_Square_Logo.jpeg",
-        caption: "TESTT asdfasdfasdfasdfsdf TESTT",
+      const err = validateFields();
+      if (Object(err).keys.length > 0) {
+        setErrors(err);
+        return;
+      }
+
+      setErrors(null);
+
+      const payload = {
+        imageUrl: progressData?.selectedImage?.url,
+        caption:
+          progressData?.selectedImage?.productCaption +
+          progressData?.selectedImage?.productHashtag.join(" "),
         company_id,
         application_id,
         createAd: false,
-      });
+      };
+      const response = await axios.post("/api/instagram/post", payload);
 
-      if (response.data.success) {
+      if (response?.data?.success) {
         console.log({ response });
-
-        // setSuccess(true);
-        // moveToNextStep();
       }
     } catch (err) {
       console.log(err.response?.data?.message || "Failed to publish post");
     }
+  };
+
+  const validateFields = () => {
+    const error = {};
+    if (!catTitle) {
+      error.catTitle = "Cat Title is required";
+    }
+
+    if (!budgetAmount || budgetAmount < 87) {
+      error.budgetAmount = "budget should be greater then ₹87.";
+    }
+
+    if (!minAge) {
+      error.minAge = "Min age is required.";
+    }
+
+    if (!maxAge) {
+      error.maxAge = "Max age is required.";
+    }
+
+    return error;
   };
 
   return (
@@ -64,38 +99,70 @@ const PublishPost = ({
               </div>
             </div>
             <div className="campaign-details-conatainer">
-              <TextField
-                id="post-caption"
-                label="CTA Title"
-                multiline
-                rows={2}
-                defaultValue={progressData?.selectedProduct?.description || ""}
-                value={productCaption}
-                onChange={(e) => setProductCaption(e.target.value)}
-              />
+              <h3>Campign Data</h3>
+              <FormControl fullWidth>
+                <InputLabel id="cat-title-demo-label">CTA Title</InputLabel>
+                <Select
+                  labelId="cat-title-demo-label"
+                  id="cat-title-demo"
+                  label="CTA Title"
+                  value={catTitle}
+                  onChange={(e) => setCatTitle(e.target.value)}
+                >
+                  {CTAtitles?.map((CTAtitle, CTAtitleIndex) => {
+                    return (
+                      <MenuItem index={CTAtitleIndex} value={CTAtitle?.value}>
+                        {CTAtitle?.title}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
 
-              <TextField
-                id="post-caption"
-                label="Budget"
-                multiline
-                rows={2}
-                defaultValue={progressData?.selectedProduct?.description || ""}
-                value={productCaption}
-                onChange={(e) => setProductCaption(e.target.value)}
-              />
-
-              <p>PDP Link</p>
+              <FormControl sx={{ mt: 2 }} fullWidth>
+                <InputLabel htmlFor="amount-budget">Amount</InputLabel>
+                <OutlinedInput
+                  id="amount-budget"
+                  type="number"
+                  min={87}
+                  startAdornment={
+                    <InputAdornment position="start">₹</InputAdornment>
+                  }
+                  label="Amount"
+                  value={budgetAmount}
+                  onChange={(e) => setBudgetAmount(e.target.value)}
+                />
+              </FormControl>
             </div>
             <div className="target-audiance-container">
-              <TextField
-                id="  "
-                label="Post Caption"
-                multiline
-                rows={2}
-                defaultValue={progressData?.selectedProduct?.description || ""}
-                value={productCaption}
-                onChange={(e) => setProductCaption(e.target.value)}
-              />
+              <h3>Target Audience</h3>
+
+              <div className="age-conatiner">
+                <TextField
+                  id="min-age"
+                  label="Min Age"
+                  type="number"
+                  slotProps={{
+                    inputLabel: {
+                      shrink: true,
+                    },
+                  }}
+                  value={minAge}
+                  onChange={(e) => setMinAge(e.target.value)}
+                />
+                <TextField
+                  id="max-age"
+                  label="Max Age"
+                  type="number"
+                  slotProps={{
+                    inputLabel: {
+                      shrink: true,
+                    },
+                  }}
+                  value={maxAge}
+                  onChange={(e) => setMaxAge(e.target.value)}
+                />
+              </div>
             </div>
           </div>
           <div className="submit-container">
@@ -103,9 +170,10 @@ const PublishPost = ({
               variant="contained"
               color="primary"
               fullWidth
-              disabled={
-                !productCaption || !productHashtag || productHashtag.length <= 0
-              }
+              disabled={() => {
+                const errs = validateFields();
+                return Object.keys(errs)?.length > 0;
+              }}
               onClick={storeAndmoveToNextStep}
             >
               Next
@@ -117,8 +185,8 @@ const PublishPost = ({
             imageUrl={progressData?.selectedImage?.url}
             profileImageUrl={authData?.instaAccountData?.profile_picture_url}
             accountName={authData?.instaAccountData?.username}
-            caption={productCaption}
-            hashtag={productHashtag?.join(" ") || ""}
+            caption={progressData?.productCaption}
+            hashtag={progressData?.productHashtag?.join(" ") || ""}
           />
         </div>
       </div>
