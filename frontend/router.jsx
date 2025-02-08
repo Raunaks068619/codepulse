a4973167d3a4db9e4932c548382ae7ba6caacb88
Priti Gediya <pritigediya@fynd-external.com>
2025-02-09 01:21:28 +0530

    fixed server redirect url issue for sub url

--- frontend/router.jsx
+++ frontend/router.jsx
@@ -3,39 +3,33 @@ import App from "./App";
 import NotFound from "./pages/NotFound";
 import VerifyAuth from "./pages/VerifyAuth";
 
-const router = createBrowserRouter([
-  {
-    path: "/company/:company_id/",
-    element: <App />,
-  },
-  {
-    path: "/company/",
-    element: <VerifyAuth />,
-  },
-  {
-    path: "/company/:company_id/application/:application_id",
-    element: <App />,
-  },
-  {
-    path: "/",
-    element: <>
-    <div>
-      its working, Please move to another working route.
-    </div>
-    </>,
-  },
-  {
-    path: "/test-page",
-    element: (
-      <>
-        <div>Hurrrey Its wokring</div>
-      </>
-    ),
-  },
-  {
-    path: "/*", // Fallback route for all unmatched paths
-    element: <NotFound />, // Component to render for unmatched paths
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/company/:company_id/",
+      element: <App />,
+    },
+    {
+      path: "/company/",
+      element: <VerifyAuth />,
+    },
+    {
+      path: "/company/:company_id/application/:application_id",
+      element: <App />,
+    },
+    {
+      path: "/test-page",
+      element: (
+        <>
+          <div>Hurrrey Its wokring</div>
+        </>
+      ),
+    },
+    {
+      path: "/*", // Fallback route for all unmatched paths
+      element: <NotFound />, // Component to render for unmatched paths
+    },
+  ]
+);
 
 export default router;
