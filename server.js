3713a0820afdd6a90e97cffdd3eecffc1c3be24f
Raunak Singh <raunaksingh@gofynd.com>
2025-02-09 20:40:10 +0530

    server.js configure MemoryStorage

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const serveStatic = require("serve-static");
 const { readFileSync } = require('fs');
 const { setupFdk } = require("@gofynd/fdk-extension-javascript/express");
-// const { SQLiteStorage } = require("@gofynd/fdk-extension-javascript/express/storage");
+const { MemoryStorage } = require("@gofynd/fdk-extension-javascript/express/storage");
 const axios = require('axios');
 const SecretsModel = require('./models/secrets.model');
 const AdvertiseModel = require('./models/advertise.model');
@@ -42,7 +42,7 @@ const fdkExtension = setupFdk({
             // If task is time taking then process it async on other process.
         }
     },
-    // storage: new SQLiteStorage(sqliteInstance, "exapmple-fynd-platform-extension"), // add your prefix
+    storage: new MemoryStorage("Raunak"), // add your prefix
     access_mode: "online",
     webhook_config: {
         api_path: "/api/webhook-events",
